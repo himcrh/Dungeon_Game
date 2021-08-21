ade00d625e8d5e4f82a7a48758fa7148087a763d
himcrh <1178990376@qq.com>
2021-08-21 23:04:14 +0800

    增加了地图绘制

--- Dungeon_Game/main.cpp
+++ Dungeon_Game/main.cpp
@@ -1,22 +1,23 @@
 #include<graphics.h>
 #include<Windows.h>
 #include<conio.h>
+#include<stdio.h>
 #include<time.h>
 #include<iostream>
 #include<stdlib.h>
+#include<fstream>
 #include"src/easy_draw.h"
 #pragma comment(lib, "winmm.lib")
-
+using namespace std;
 #define High 440
 #define Width 880
 #define Unit 40
-using namespace std;
+int map[10][13][13];
 
 IMAGE img_background;
 IMAGE img_test;
 IMAGE img_floor;
 IMAGE img_wall;
-int map[13][13];
 
 void test(void); //测试用
 void start_initial(void);  //初始化，加载文件
@@ -35,9 +36,19 @@ void start_initial() {
   srand(time(NULL));//随机时间种子
 
   //载入地图信息
-  fstream Map_file.open("Map_file.txt", ios::in);
-
-
+  fstream Map_file;
+  Map_file.open("Map_file.txt",ios::in);
+  if (!Map_file) return;
+  freopen("Map_file.txt", "r", stdin);
+  int n; scanf_s("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= 10; j++) {
+      for (int k = 0; k <= 10; k++) {
+        scanf_s("%d", &map[i][j][k]);
+      }
+    }
+  }
+  
   initgraph(Width, High);
   setbkmode(TRANSPARENT);
   loadimage(&img_test, _T("images\\test.png"));
@@ -57,12 +68,14 @@ void show() {
   //背景绘图
   for (int i = 0; i <= 10; i++) {
     for (int j = 0; j <= 10; j++) {
-      drawAlpha(i * 40, j * 40, &img_floor);
-      //测试周围一圈墙体
-      if (i == 0 || i == 10 || j == 0 || j == 10) {
-        drawAlpha(i * 40, j * 40, &img_wall);
+      switch (map[1][i][j]) {
+        case 0:
+          drawAlpha(j * 40, i * 40, &img_floor);
+          break;
+        case 1:
+          drawAlpha(j * 40, i * 40, &img_wall);
+          break;
       }
-         
     }
   }
 
