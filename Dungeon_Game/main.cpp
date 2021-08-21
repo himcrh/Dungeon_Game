11b8d7edbb729c9022c4b6df4be58e827c620b7b
himcrh <1178990376@qq.com>
2021-08-21 18:04:31 +0800

    增加了测试代码，解决了png背景透明无法显示的问题

---
+++ Dungeon_Game/main.cpp
@@ -0,0 +1,39 @@
+#include<graphics.h>
+#include<Windows.h>
+#include<conio.h>
+#include<time.h>
+#include<iostream>
+#include<stdlib.h>
+#include"src/easy_draw.h"
+#pragma comment(lib, "winmm.lib")
+
+#define High 600
+#define Width 600
+using namespace std;
+
+IMAGE img_background;
+IMAGE img_test;
+
+void test(void); //测试用
+void start_initial(void);  //初始化，加载文件
+
+int main() {
+  start_initial();
+  test();
+  return 0;
+}
+
+void start_initial() {
+  srand(time(NULL));//随机时间种子
+  initgraph(Width, High);
+  setbkmode(TRANSPARENT);
+  loadimage(&img_background, _T("images\\bg2.png"));
+  loadimage(&img_test, _T("images\\test.png"));
+  BeginBatchDraw();
+}
+void test() {
+  putimage(0, 0, &img_background);
+  drawAlpha(&img_test,0, 0);
+  FlushBatchDraw();
+  Sleep(5000000);
+}
