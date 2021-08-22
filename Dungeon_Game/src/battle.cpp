e89214014b053740dbb313a15f02770a922156df
himcrh <1178990376@qq.com>
2021-08-22 22:17:09 +0800

    实现了攻击对战功能

--- Dungeon_Game/src/battle.cpp
+++ Dungeon_Game/src/battle.cpp
@@ -1,6 +1,6 @@
 #include"battle.h"
 
-bool vs_cmp(int cur_monster) {
+bool vs_cmp(int cur_monster,int x,int y) {
   if (cur_monster > 90 || cur_monster <= 10)
     return true;
   cur_monster -= 10;
@@ -13,7 +13,29 @@ bool vs_cmp(int cur_monster) {
   cnt_b = Hero.HP / Monsters[cur_monster].attack;
   if (Hero.HP % Monsters[cur_monster].attack)
     cnt_b++;
-  if (cnt_a <= cnt_b)
+  if (cnt_a <= cnt_b) {
+    battle_details(cur_monster,x,y);
     return true;
+  }
+    
   return false;
 }
+
+void battle_details(int cur_monster,int x,int y) {
+  Monster cur_M = Monsters[cur_monster];
+  //提取出一个moster
+  while (cur_M.HP > 0) {
+    cur_M.HP -= Hero.attack;
+    Hero.HP -= cur_M.attack;
+    draw_backgraund();
+    draw_information();
+    draw_fixobject();
+    draw_unfixobject();
+    drawAlpha(Hero.position_x, Hero.position_y, &img_hero[Hero.dir]);
+    FlushBatchDraw();
+    Sleep(200);
+  }
+  map[Hero.stair][x][y] = 0;
+}
+
+
--- Dungeon_Game/src/battle.h
+++ Dungeon_Game/src/battle.h
@@ -1,6 +1,6 @@
 #ifndef BATTLE
 #define BATTLE
 #include "global.h"
-bool vs_cmp(int cur_monster);//判断是否能打过怪物
-
+bool vs_cmp(int cur_monster,int x,int y);//判断是否能打过怪物
+void battle_details(int cur_monster,int x,int y);
 #endif  // !BATTLE
